e0a5ae34c5459297211b212df4341c755fbc9eee
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-09-09 16:25:34 -0300

    implementando os digitos dos botoes e algumas funções

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,22 +7,36 @@ import {
 } from 'react-native'
 
 const styles = StyleSheet.create({
-    button:{
-        fontSize:40,
+    button: {
+        fontSize: 40,
         height: Dimensions.get("window").width / 4,
         width: Dimensions.get("window").width / 4,
         padding: 20,
-        backgroundColor:'#f0f0f0',
+        backgroundColor: '#f0f0f0',
         textAlign: 'center',
-        borderWidth:1,
-        borderColor:"#888",
+        borderWidth: 1,
+        borderColor: "#888",
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
     }
 })
 
-export default props =>{
+export default props => {
+    const stylesButton = [styles.button]
+    if(props.double) stylesButton.push(styles.buttonDouble)
+    if(props.triple) stylesButton.push(styles.buttonTriple)
+    if(props.operation) stylesButton.push(styles.operationButton)
     return (
-        <TouchableHighlight onPress={props.onCLick}>
-            <Text style={styles.button}>{props.label}</Text>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
