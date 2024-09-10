4b7a6dadfd7f34fb957ddbfe3fc54c00fee43576
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-09-10 17:37:12 -0300

    inicio da logica da calculadora

--- App.js
+++ App.js
@@ -3,17 +3,38 @@ import { Platform, StyleSheet, Text, View } from "react-native";
 import Button from "./src/components/Button";
 import Display from "./src/components/Display";
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
+
 export default class App extends Component {
-  state = {
-    displayValue: '0'
-  }
+  state = { ...initialState }
 
   addDigit = n => {
-    this.setState({ displayValue: n })
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
+
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: '0' })
+    this.setState({ ...initialState })
   }
 
   setOperation = operation => {
@@ -29,14 +50,14 @@ export default class App extends Component {
           <Button label='/' operation onClick={this.setOperation} />
           <Button label='7' onClick={this.addDigit} />
           <Button label='8' onClick={this.addDigit} />
-          <Button label='9' onClick={this.addDigit}/>
+          <Button label='9' onClick={this.addDigit} />
           <Button label='*' operation onClick={this.setOperation} />
           <Button label='4' onClick={this.addDigit} />
           <Button label='5' onClick={this.addDigit} />
           <Button label='6' onClick={this.addDigit} />
           <Button label='-' operation onClick={this.setOperation} />
           <Button label='1' onClick={this.addDigit} />
-          <Button label='2' onClick={this.addDigit}/>
+          <Button label='2' onClick={this.addDigit} />
           <Button label='3' onClick={this.addDigit} />
           <Button label='+' operation onClick={this.setOperation} />
           <Button label='0' double onClick={this.addDigit} />
